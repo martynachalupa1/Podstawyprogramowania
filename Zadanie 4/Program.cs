16780caa105adfbce5fc9cde19671d332cd1756c
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-03-20 21:58:25 +0100

    Wprowadzenie

---
+++ Zadanie 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czesc");
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
