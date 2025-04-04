b57ef67a4201bb74d12878e2dd9797fad0f6a2d4
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-04 14:28:42 +0200

    While

---
+++ Zad1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Wczytaj z klawiatury dwie liczby naturalne a i b(0 < a < b).Wyznacz wszystkie liczby parzyste z przedziału[a, b].Użyj pętli while.
+            int a, b, i;
+            Console.WriteLine("Podaj a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b =");
+            b = Convert.ToInt32(Console.ReadLine());
+            i = a;
+            while(i<=b)
+            {
+                if (i % 2 == 0) Console.WriteLine("{0}, ", i);
+                i++;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zad3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zad3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zad3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("8976caea-79b5-4e9e-b7b8-82dcc6f3809b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
