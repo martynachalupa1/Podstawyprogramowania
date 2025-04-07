e90912b1322c8dd12b3e98da87a355e8aaf48f59
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-07 10:04:36 +0200

    warunkowe

--- Zadanie2-3/Program.cs
+++ Zadanie2-3/Program.cs
@@ -10,7 +10,27 @@ namespace Zadanie2_3
     {
         static void Main(string[] args)
         {//Napisać program wczytujący trzy liczby i wypisujący na ekranie wartość najmniejszej i największej z tych liczb. 
-            
+            int a, b, c;
+            Console.WriteLine("Podaj liczbę a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe c=");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a >= b && a >= c)
+                Console.WriteLine("{0} jest największa z tych liczb", a);
+            else if (b >= a && b >= c)
+                Console.WriteLine("{0} jest największa z tych liczb", b);
+            else
+                Console.WriteLine("{0} jest najwieksza z tych liczb", c);
+            if (a <= b && a <= c)
+                Console.WriteLine("{0} jest najmniejsza z tych liczb", a);
+            else if (b <= a && b <= c)
+                Console.WriteLine("{0} jest najmniejsza z tych liczb", b);
+            else
+                Console.WriteLine("{0} jest najmniejsza z tych liczb", c);
+            Console.ReadKey(true);
+
         }
     }
 }
