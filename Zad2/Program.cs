932d26b62c450435f9977476012abaa3f1599169
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-07 15:55:45 +0200

    Reapply "While"
    This reverts commit 0e0c9ee56b7204051350e7f6a755026a5f1fabc7.

---
+++ Zad2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Dany jest wzór ciągu a(n)=2n-1. Napisz program wczytujący liczbę całkowitą dodatnią n i wypisujący na ekranie kolejne wyrazy ciągu: a(1), a(2), …, a(n).Użyj pętli while.
+            int an, i, n;
+            Console.WriteLine("Podaj n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            while (i <= n)
+            {
+                an = 2 * i - 1;
+                Console.WriteLine("a({0}) ={1}",i,an);
+                i++;
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
