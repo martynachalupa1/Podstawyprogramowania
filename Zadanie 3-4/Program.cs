66347eaa08190dc627ad8ccb711119db99bc9361
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-10 18:22:44 +0200

    koniec

---
+++ Zadanie 3-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Wczytaj dwie liczby naturalne dodatnie n i k. Narysuj z gwiazdek na ekranie prostokąt posiadający n wierszy i k kolumn. Użyj pętli for. 
+            int n, k,i,j;
+            Console.WriteLine("Podaj liczbę n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę k=");
+            k = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= k; j++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
+            
+            Console.ReadKey(true);
+
+        }
+    }
+}
