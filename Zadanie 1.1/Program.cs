811fc887ba6e7369b46205f1e3e3ec7c29f01f00
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-10 14:53:14 +0200

    kolokwium

---
+++ Zadanie 1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Napisać program wypisujący na ekranie tekst "Witaj Świecie".
+            Console.WriteLine("Witaj Świecie");
+            Console.ReadKey(true);
+        }
+    }
+}
