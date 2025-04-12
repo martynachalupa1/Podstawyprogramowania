1d3f18d9c0a331726ae804daae54505ccd220c33
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-12 20:09:41 +0200

    for

---
+++ Zadania3-9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania3_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Wypisz tabliczkę mnożenia do 100 w formie tabelki:
+            int i, j;
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= 10; j++)
+                    Console.Write("{0} ", j * i);
+                Console.WriteLine("");
+            }
+            Console.ReadKey(true);
+            
+
+
+        }
+    }
+}
