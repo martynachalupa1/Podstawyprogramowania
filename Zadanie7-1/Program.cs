5d5b1978fe7c1ca01e7891724f6be876ea0c7cce
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-14 13:43:27 +0200

    metody

---
+++ Zadanie7-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie7_1
+{
+    internal class Program
+    {//Napisz funkcję o nazwie suma wyznaczającą sumę jej dwóch argumentów będących liczbami całkowitymi.Napisz program wykorzystujący funkcję suma.
+        //funkcja oblicza sumę jej argumentów
+        static int suma (int a, int b)
+            {
+            return a + b;
+            }
+        //testujemy naszą funkcję 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2+5={0}", suma(2, 5));
+            Console.ReadKey(true);
+        }
+    }
+}
