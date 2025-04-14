1bfa1c70535bb58e64bbc78c00c296c725707472
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-14 12:16:37 +0200

    koniec

---
+++ Zadanie4-10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Napisać program sumujący kolejne liczby całkowite podawane przez użytkownika, aż do napotkania zera. Wypisz otrzymaną sumę na ekranie.Użyj pętli do -while.
+            int  i, suma=0;
+            do
+            {
+                Console.WriteLine("Podaj liczbę całkowitą i:");
+                i = Convert.ToInt32(Console.ReadLine());
+                suma += i;
+            } while (i!=0);
+            Console.WriteLine("Suma tych liczb całkowitych wynosi {0}", suma);
+            Console.ReadKey(true);
+        }
+    }
+}
