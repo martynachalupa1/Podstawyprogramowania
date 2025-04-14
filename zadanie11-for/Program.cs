8dc2cec722a9f863f0c3f192b88d389cd1b2ebdf
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-04-14 12:40:52 +0200

    konieec

---
+++ zadanie11-for/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie11_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, suma = 0, liczba = 0;
+            Console.Write("Podaj liczbe n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i=1;i<=n;i++)
+            {
+                liczba = 2 * i - 1;
+                suma += liczba;
+            }
+            Console.WriteLine("Suma {0} liczb nieparzystych to {1}", n, suma);
+            Console.ReadKey(true);
+        }
+    }
+}
