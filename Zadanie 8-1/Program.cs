0a7668061ec0e7035cdcfb94bde7e0f83c98c176
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-05-12 14:29:40 +0200

    rekurencja

---
+++ Zadanie 8-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_8_1
+{
+    internal class Program
+    {
+        static long silnia(int n)
+        {
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny argument");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
+        }
+    }
+}
