6252f1f63c350041c520baa9018bc43379da858a
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-05-25 18:25:10 +0200

    srednia

---
+++ Chat2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat2
+{
+    internal class Program
+    {
+        static double srednia(int[]tablica)
+        {
+            double suma = 0;
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                suma += tablica[i];
+            }
+            return suma / tablica.Length;
+        }
+        static void Main(string[] args)
+        {
+            int[] tablica = { 1, 3, 5 };
+            Console.Write(srednia(tablica));
+            Console.ReadKey(true);
+        }
+    }
+}
