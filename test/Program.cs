63dd8a89fb79e0f21972a4a6ce939debb31130bd
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-05-26 12:58:02 +0200

    suma

---
+++ test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static int[] odwrotnosc(int[]tab)
+        {
+            int[] nowa = new int[tab.Length];
+            for (int i=0;i<tab.Length;i++)
+            {
+                nowa[i] = tab[tab.Length - 1 - i];
+            }
+            return nowa;
+        }
+        static void Main(string[]args)
+        {
+            int[] tablica = { 1, 2, 3, 4, 5 };
+            Console.Write("Odwrotnie: {0}", odwrotnosc(tablica));
+            Console.ReadKey(true);
+        }
+    }
+}
