b9b7f086036c7a816fcd94376ff68b0e29e935b4
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-06-20 17:40:25 +0200

    rekurencja

---
+++ Zadanie8_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie8_2
+{
+    internal class Program
+    {
+        static long fib(long n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Błędny parametr!");
+            else Console.WriteLine("Fib({0})={1}", n, fib(n));
+            Console.ReadKey(true);
+        }
+    }
+}
