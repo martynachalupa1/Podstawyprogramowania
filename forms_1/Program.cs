ce5d329514d2ff666c62e215d4948cd7dbe13aa1
martynachalupa1 <m.chalupa.016@studms.ug.edu.pl>
2025-06-20 17:57:50 +0200

    koniec

---
+++ forms_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forms_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, n, suma = 0;
+            Console.Write("Podaj n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++) suma += i;
+
+            string folder = @"c:\tmp";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            // zapis do pliku
+            string nazwa = @"c:\tmp\wynik1.txt";
+            FileStream plik = new FileStream(nazwa, FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter zapis = new StreamWriter(plik);
+            zapis.WriteLine(suma);
+            zapis.Close();
+            plik.Close();
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ forms_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forms_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nazwa = @"c:\tmp\liczby1.txt";
+            double a, b, c, suma = 0;
+            FileStream plik = new FileStream(nazwa,
+                                FileMode.Open, FileAccess.Read, FileShare.None);
+            System.IO.StreamReader odczyt = new StreamReader(plik);
+
+
+            a = Convert.ToDouble(odczyt.ReadLine());
+            b = Convert.ToDouble(odczyt.ReadLine());
+            c = Convert.ToDouble(odczyt.ReadLine());
+            suma = a + b + c;
+
+            odczyt.Close();
+            plik.Close();
+
+            Console.WriteLine("Suma liczb w pliku wynosi {0}", suma);
+            Console.ReadKey(true);
+        }
+    }
+
+}
